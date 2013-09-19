b50f89800d4d3baeaf6bb4a8d39c604594201cc2
wanwu <paul.wanwu@gmail.com>
2013-09-19 11:03:20 +0100

    Added questions, and interaction to cycle through question display via a 'Next' button

--- application.js
+++ application.js
@@ -5,3 +5,60 @@
  * Time: 17:41
  * To change this template use File | Settings | File Templates.
  */
+
+var questions = [
+                {   question: "What is the population of Brazil?",
+                    choice: ["145 Million", "175 million", "195 million", "205 million"],
+                    correctAnswer: 0
+                },
+                {   question: "What is 27*14?",
+                    choice: ["485", "634", "408", "528"],
+                    correctAnswer: 2
+                },
+                {   question: "What is the busiest train station in the world?",
+                    choice: ["Grand Central, NY", "Shibuya, Tokyo", "Beijing Central, Chine", "Gard du Nord, Paris"],
+                    correctAnswer: 0
+                },
+                {   question: "What is the longest river?",
+                    choice: ["Nile", "Amazon", "Mississippi", "Mekong"],
+                    correctAnswer: 0
+                },
+                {   question: "What is the busiest tube station in the London?",
+                    choice: ["Waterloo", "Baker Street", "Kings Cross", "Victoria"],
+                    correctAnswer: 0
+                }
+];
+
+var currentQuestion = 0;
+
+$(document).ready(function() {
+
+    // Display the first question
+    displayCurrentQuestion();
+
+    // On clicking next, display the next question
+    $(this).find(".nextButton").on("click", function() {
+        currentQuestion++;
+        if (currentQuestion < questions.length) {
+            displayCurrentQuestion();
+        } else {
+            displayScore();
+        }
+    });
+});
+
+function displayCurrentQuestion() {
+    var question = questions[currentQuestion].question;
+    console.log(question);
+    $(document).find(".question").text(question);
+}
+
+function resetQuiz() {
+    currentQuestion = 0;
+    quizScore = 0;
+}
+
+function displayScore() {
+    resetQuiz();
+}
+
