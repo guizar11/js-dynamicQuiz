a3bb36e68e7cbd772d69dd80552219e87fda4403
wanwu <paul.wanwu@gmail.com>
2013-09-20 09:10:09 +0100

    Re-factored code. Added code to remove choices from choiceList and reset the quiz.

--- application.js
+++ application.js
@@ -30,54 +30,79 @@ var questions = [
 ];
 
 var currentQuestion = 0;
+var correctAnswers = 0;
 
 $(document).ready(function() {
 
     // Display the first question
+    console.log("Current Question: " + currentQuestion);
+
     displayCurrentQuestion();
 
     // On clicking next, display the next question
     $(this).find(".nextButton").on("click", function() {
-        currentQuestion++;
+
         if (currentQuestion < questions.length) {
             displayCurrentQuestion();
         } else {
+            console.log("Reset Quiz");
+
             displayScore();
+            resetQuiz();
+
+            // This adds a new event to nextButton
+            // Instead we need to remove nextButton, display playAgain button
+            // If you click that - reset the quiz, delete the playAgain button
+            // And display the nextButton
+
+//            $(document).find(".nextButton").remove();
+//            $(document).find(".nextButton").on("click", function() {
+//                resetQuiz();
+//            });
         }
+
+        currentQuestion++;
     });
+
 });
 
-// This displays the current question AND the choices
 
-// TODO: Clean up the js stuff. We should not be referring to document
-// TODO: Find the right node and ensure you adding <li> properly
-// TODO: Delete all nodes before you go to the next question
-// TODO: refactor it all in jQuery
 // TODO: do a version using Pure JS - no jQuery
 // TODO: Add clean CSS and style it nicely
 
+// This displays the current question AND the choices
 function displayCurrentQuestion() {
+
+    console.log("In display current Question");
+
     var question = questions[currentQuestion].question;
-    console.log(question);
-    $(document).find(".question").text(question);
-
+    var questionClass = $(document).find(".quiz > .question");
+    var choiceList = $(document).find(".quiz > .choiceList");
     var numChoices = questions[currentQuestion].choices.length;
+
+    // Set the questionClass text to the current question
+    $(questionClass).text(question);
+
+    // Remove all current <li> elements (if any)
+    $(choiceList).find("li").remove();
+
     for ( i = 0; i < numChoices; i ++) {
+        choice = questions[currentQuestion].choices[i];
+        choiceTag = $('<li>' + choice + '</li>');
 
-        var choice = questions[currentQuestion].choices[i];
-        var choiceTag = $('<li>' + choice + '</li>');
-        console.log(choice);
-
-        $(document).find("ul").append(choiceTag);
+        // Append the choices to the choiceList
+        $(choiceList).append(choiceTag);
     }
+
+    $(document).find(".nextButton").text("Next question");
 }
 
 function resetQuiz() {
     currentQuestion = 0;
-    quizScore = 0;
+    correctAnswers = 0;
 }
 
 function displayScore() {
-    resetQuiz();
+    $(document).find(".quiz > .result").text("You scored: " + correctAnswers + " out of: " + questions.length);
 }
 
