09c25819cd0f9a01c8593db8ef1442834ea37ab9
wanwu <paul.wanwu@gmail.com>
2013-09-21 20:34:55 +0100

    Updated CSS and added functionality to check whether an asnwer has been selected.

--- application.js
+++ application.js
@@ -8,8 +8,8 @@
 
 var questions = [
                 {   question: "What is the population of Brazil?",
-                    choices: ["145 Million", "175 million", "195 million", "205 million"],
-                    correctAnswer: 0
+                    choices: ["145 million", "199 million", "182 million", "205 million"],
+                    correctAnswer: 1
                 },
                 {   question: "What is 27*14?",
                     choices: ["485", "634", "408", "528"],
@@ -17,10 +17,10 @@ var questions = [
                 },
                 {   question: "What is the busiest train station in the world?",
                     choices: ["Grand Central, NY", "Shibuya, Tokyo", "Beijing Central, Chine", "Gard du Nord, Paris"],
-                    correctAnswer: 0
+                    correctAnswer: 1
                 },
                 {   question: "What is the longest river?",
-                    choices: ["Nile", "Amazon", "Mississippi", "Mekong"],
+                    choices: ["Nile", "Amazon", "Mississippi", "Yangtze"],
                     correctAnswer: 0
                 },
                 {   question: "What is the busiest tube station in the London?",
@@ -36,19 +36,39 @@ $(document).ready(function() {
 
     // Display the first question
     displayCurrentQuestion();
+    $(this).find(".quizMessage").hide();
 
     // On clicking next, display the next question
     $(this).find(".nextButton").on("click", function() {
-        if (currentQuestion < questions.length) {
-            displayCurrentQuestion();
-        } else {
-            displayScore();
-            $(document).find(".nextButton").toggle();
-            $(document).find(".playAgainButton").toggle();
+
+        value = $("input[type='radio']:checked").val();
+
+        if(value == undefined) {
+            $(document).find(".quizMessage").text("Please select an answer");
+            $(document).find(".quizMessage").show();
+        }
+        else {
+            // TODO: Remove any message -> not sure if this is efficient to call this each time....
+            $(document).find(".quizMessage").hide();
+
+            if (value == questions[currentQuestion].correctAnswer) {
+                correctAnswers++;
+            }
+
+            currentQuestion++; // Since we have already displayed the first question on DOM ready
+            if (currentQuestion < questions.length) {
+                displayCurrentQuestion();
+            } else {
+                displayScore();
+                $(document).find(".nextButton").toggle();
+                $(document).find(".playAgainButton").toggle();
+            }
         }
     });
 
-    $(document).find(".playAgainButton").on("click", function() {
+    // Set up event for the playAgain button and also toggle the nextButton
+    // TODO: Just change the button text instead to refactor button appropriately
+    $(this).find(".playAgainButton").on("click", function() {
         $(document).find(".nextButton").toggle();
         $(document).find(".playAgainButton").toggle();
         resetQuiz();
@@ -76,21 +96,23 @@ function displayCurrentQuestion() {
     var choice, choiceTag;
     for ( i = 0; i < numChoices; i ++) {
         choice = questions[currentQuestion].choices[i];
-        choiceTag = $('<li>' + choice + '</li>');
-
-        // Append the choices to the choiceList
-        $(choiceList).append(choiceTag);
+        $('<li><input type="radio" value=' + i + ' name="dynradio" />' + choice + '</li>').appendTo(choiceList);
     }
-
-    currentQuestion++;
 }
 
 function resetQuiz() {
     currentQuestion = 0;
     correctAnswers = 0;
+    hideScore();
 }
 
 function displayScore() {
     $(document).find(".quiz > .result").text("You scored: " + correctAnswers + " out of: " + questions.length);
+    $(document).find(".quiz > .result").show();
+}
+
+function hideScore() {
+    // TODO: Doesn't work...
+    $(document).find(".result").hide();
 }
 
