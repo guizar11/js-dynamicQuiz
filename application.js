7d8956865e36d5575aab1e77b4e5538ca34d00c1
wanwu <paul.wanwu@gmail.com>
2013-09-22 15:04:16 +0100

    Finished basic Javascript quiz using jQuery.

--- application.js
+++ application.js
@@ -31,6 +31,7 @@ var questions = [
 
 var currentQuestion = 0;
 var correctAnswers = 0;
+var quizOver = false;
 
 $(document).ready(function() {
 
@@ -40,39 +41,41 @@ $(document).ready(function() {
 
     // On clicking next, display the next question
     $(this).find(".nextButton").on("click", function() {
+        if (! quizOver ) {
 
-        value = $("input[type='radio']:checked").val();
+            value = $("input[type='radio']:checked").val();
 
-        if(value == undefined) {
-            $(document).find(".quizMessage").text("Please select an answer");
-            $(document).find(".quizMessage").show();
-        }
-        else {
-            // TODO: Remove any message -> not sure if this is efficient to call this each time....
-            $(document).find(".quizMessage").hide();
-
-            if (value == questions[currentQuestion].correctAnswer) {
-                correctAnswers++;
+            if(value == undefined) {
+                $(document).find(".quizMessage").text("Please select an answer");
+                $(document).find(".quizMessage").show();
             }
+            else {
+                // TODO: Remove any message -> not sure if this is efficient to call this each time....
+                $(document).find(".quizMessage").hide();
 
-            currentQuestion++; // Since we have already displayed the first question on DOM ready
-            if (currentQuestion < questions.length) {
-                displayCurrentQuestion();
-            } else {
-                displayScore();
-                $(document).find(".nextButton").toggle();
-                $(document).find(".playAgainButton").toggle();
+                if (value == questions[currentQuestion].correctAnswer) {
+                    correctAnswers++;
+                }
+
+                currentQuestion++; // Since we have already displayed the first question on DOM ready
+                if (currentQuestion < questions.length) {
+                    displayCurrentQuestion();
+                } else {
+                    displayScore();
+//                    $(document).find(".nextButton").toggle();
+//                    $(document).find(".playAgainButton").toggle();
+                    // Change the text in the next button to ask if user wants to play again
+                    $(document).find(".nextButton").text("Play Again?");
+                    quizOver = true;
+                }
             }
+        } else { // quiz is over and clicked the next button (which now displays 'Play Again?'
+            quizOver = false;
+            $(document).find(".nextButton").text("Next Question");
+            resetQuiz();
+            displayCurrentQuestion();
+            hideScore();
         }
-    });
-
-    // Set up event for the playAgain button and also toggle the nextButton
-    // TODO: Just change the button text instead to refactor button appropriately
-    $(this).find(".playAgainButton").on("click", function() {
-        $(document).find(".nextButton").toggle();
-        $(document).find(".playAgainButton").toggle();
-        resetQuiz();
-        displayCurrentQuestion();
     });
 
 });
@@ -83,8 +86,8 @@ function displayCurrentQuestion() {
     console.log("In display current Question");
 
     var question = questions[currentQuestion].question;
-    var questionClass = $(document).find(".quiz > .question");
-    var choiceList = $(document).find(".quiz > .choiceList");
+    var questionClass = $(document).find(".quizContainer > .question");
+    var choiceList = $(document).find(".quizContainer > .choiceList");
     var numChoices = questions[currentQuestion].choices.length;
 
     // Set the questionClass text to the current question
@@ -93,7 +96,7 @@ function displayCurrentQuestion() {
     // Remove all current <li> elements (if any)
     $(choiceList).find("li").remove();
 
-    var choice, choiceTag;
+    var choice;
     for ( i = 0; i < numChoices; i ++) {
         choice = questions[currentQuestion].choices[i];
         $('<li><input type="radio" value=' + i + ' name="dynradio" />' + choice + '</li>').appendTo(choiceList);
@@ -107,8 +110,8 @@ function resetQuiz() {
 }
 
 function displayScore() {
-    $(document).find(".quiz > .result").text("You scored: " + correctAnswers + " out of: " + questions.length);
-    $(document).find(".quiz > .result").show();
+    $(document).find(".quizContainer > .result").text("You scored: " + correctAnswers + " out of: " + questions.length);
+    $(document).find(".quizContainer > .result").show();
 }
 
 function hideScore() {
